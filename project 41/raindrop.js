28a249901e11130b32319075ea969bf6acc7ce26
bunnykiller003 <63455593+bunnykiller003@users.noreply.github.com>
2020-07-04 13:11:23 +0530

    Add files via upload

---
+++ project 41/raindrop.js
@@ -0,0 +1,8 @@
+class raindrop{
+constructor(x,y){
+this.image=loadImage("raindrop.jpg")
+
+raindrop.velocity.y(-5);
+
+}
+}
---
+++ project 41/sketch.js
@@ -0,0 +1,17 @@
+var bruce;
+var rain;
+
+
+function preload(){
+ this.image=loadImage("bruce.jpg");
+}
+
+function setup(){
+   
+    
+}
+
+function draw(){
+    
+}   
+
